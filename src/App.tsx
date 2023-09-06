a2c4562308eac70560e567a086323eec0551d37a
priyanshu983 <priyanshutiwari6789@gmail.com>
2023-09-06 04:30:34 +0000

    routes made

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import { Login } from "./pages/login/Login";
 import CartContext from "./contexts/CartContext";
+import Home from "./pages/home/Home";
+import Signup from "./pages/signup/Signup";
 
 function App() {
   return (
@@ -10,7 +12,10 @@ function App() {
         <CartContext>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            {/* <Route path="/products" element={<Login />} /> */}
           </Routes>
         </CartContext>
       </BrowserRouter>
