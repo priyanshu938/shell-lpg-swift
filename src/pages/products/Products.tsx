50e769dd7d383275b32c23491402d7939a9b06bd
priyanshu983 <priyanshutiwari6789@gmail.com>
2023-09-07 04:38:53 +0000

    products search made

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import styles from "./Products.module.css";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <>
+      <div className={[styles.searchBox, "container"].join(" ")}>
+        <SearchIcon />
+        <input
+          type="search"
+          name=""
+          id=""
+          className={styles.inputSearch}
+          placeholder="Search..."
+        />
+      </div>
+      <div className="productsContainer"></div>
+    </>
+  );
 };
 
 export default Products;
